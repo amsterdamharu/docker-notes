2760ad52dad92bdeb77d8ee5c76398fa8b655ed6
Harm Meijer <amsterdamharu@hotmail.com>
2015-06-08 14:29:57 +0800

    more code to passport

--- passport/README.md
+++ passport/README.md
@@ -1,9 +1,12 @@
 from: https://www.youtube.com/watch?v=twav6O53zIQ
 
-TODO: figure out the idea behind serialize and desirialize
-TODO: get user from mongodb and save session in redis
+TODO: create a function to only get one record (mongoFindOne).
+TODO: make sure user name is unuque
 
-start squid:
+set npm proxy: npm config set proxy http://172.17.42.1:3128/
+set proxy in bash: http_proxy=http://172.17.42.1:3128
+
+# start squid:
 docker run -d \
          -v $(pwd)/../squid/squid.conf:/etc/squid3/squid.conf \
          -v $(pwd)/../squid/cachedir:/cachedir \
@@ -13,8 +16,34 @@ docker run -d \
          --net host \
          squid:2015-05 \
          bash /root/run.sh
+# start mongodb:
+docker run -d \
+         -v $(pwd)/../mongodb/log:/var/log/mongodb/ \
+         -v $(pwd)/../mongodb/db:/data/db \
+         -v $(pwd)/../mongodb/mongod.conf:/etc/mongod.conf \
+         -p 27017:27017 \
+         --name mongodb \
+         mongodb:dev mongod
+# start redis:
+        docker run -d \
+         -v $(pwd)/../redis/log:/var/log/redis \
+         -v $(pwd)/../redis/db:/data/db \
+         -v $(pwd)/../redis/redis.conf:/etc/redis/redis.conf \
+         -p 6379:6379 \
+         --name redis \
+         redis:dev redis-server
+# start orion ide
+        docker run -d \
+         -v ~/orion:/orion \
+         -v $(pwd)/app:/orion/serverworkspace/am/amsterdamharu/OrionContent/test \
+         -w /orion \
+         -u 1000 \
+         -p 9999:9999 \
+         --name orion \
+         centos:dev \
+         eclipse/orion
 
-start node:
+# start node:
 docker run -it --rm \
          -v $(pwd)/app:/app \
          -w /app \
@@ -25,11 +54,11 @@ docker run -it --rm \
          node:dev bash
 
 
-start mongodb:
-docker run -d \
-         -v $(pwd)/../mongodb/log:/var/log/mongodb/ \
-         -v $(pwd)/../mongodb/db:/data/db \
-         -v $(pwd)/../mongodb/mongod.conf:/etc/mongod.conf \
-         -p 27017:27017 \
-         --name mongodb \
-         mongodb:dev mongod
+start mongodb-express
+        docker run -it --rm \
+         -v $(pwd)/../mongodb-express/app:/app \
+         -v $(pwd)/../mongodb-express/config.js:/app/config.js \
+         -w /app \
+         -p 8888:80 \
+         --name mongo-express \
+         node:dev
--- passport/app/app.js
+++ passport/app/app.js
@@ -4,14 +4,22 @@ var express = require('express'),
   bodyParser = require('body-parser'),
   cookieParser = require('cookie-parser'),
   session = require('express-session'),
+  RedisStore = require('connect-redis')(session),
+  bcrypt = require('bcryptjs'),
   app = express(),
   secret = process.env.SESSION_SECRET || 'hello_world',
+  MongoClient = require('mongodb').MongoClient,
+  users = null,
   port = process.env.PORT || 80;
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(cookieParser());
 app.use(session({
 	secret:secret,
+	store: new RedisStore({
+		host:'172.17.42.1',
+		port:'6379'
+	}),
 	resave:false,
 	saveUninitialized:false
 }));
@@ -19,23 +27,61 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 function verifyCredentials(username, password, done) {
-    // Pretend this is using a real database!
-    if (true) {
-        done(null, { id: username, name: username });
-    } else {
-        done(null, null);
-    }
+  // Pretend this is using a real database!
+    mongoFind(users,{name:username})
+    .then(function resolve(data){
+      if(!data.length){
+		  done(null,null);return;
+	  }
+	  checkPassword(data[0].password, password)
+	  .then(function resolve (result){
+	    if(result===true){
+		    done(null, { id: data[0]._id, name: data[0].name });return;
+		}
+		done(null,null);
+	  },function reject(err){
+		console.log('we have an error:',err);
+	    done(err,null);
+	  });
+	},function reject(err){
+		console.log('an error:',err);
+		done(err,null);
+	});
 }
 passport.use(new passportLocal.Strategy(verifyCredentials));
 passport.serializeUser(function(user, done) {
 	//save somewhere (redis)
-    done(null, user.id);
+    done(null, user);
 });
-passport.deserializeUser(function(id, done) {
+passport.deserializeUser(function(user, done) {
     // get it from somewhere
-    done(null, { id: id, name: 'harm' });
+    console.log('and id is:',user);
+    done(null, user);
 });
 
+function checkPassword(hash, password){
+	return new Promise(function(resolve, reject){
+	  bcrypt.compare(password, hash, function(err, res) {
+	    if(err){
+		  reject(err);return;
+		}
+        resolve(res);
+      });
+	});
+};
+
+bcrypt.genSalt(10, function(err, salt) {
+    console.log('salt is:',salt);
+    bcrypt.hash("harm", salt, function(err, hash) {
+        console.log('hash is:',hash);
+        checkPassword(hash,'harm')
+        .then(function resolve(res){
+		  console.log('check returned:',res);
+		},function reject(err){
+		  console.log('error checking password:',err);
+		});
+    });
+});
 
 app.use(express.static('static'));
 
@@ -55,3 +101,35 @@ app.get('/logout',function(req,res){
 app.listen(port, function() {
 	console.log('http://localhost:'+port);
 });
+
+
+function mongoFind (collection, query){
+  var p = new Promise(function(resolve,reject){
+    collection.find(query).toArray(function(err,results){
+	  if(err){
+        reject(err);return;
+      }
+      resolve(results);
+	});
+  })
+  return p;
+}
+
+  MongoClient.connect('mongodb://172.17.42.1:27017/app', function(err, db) {
+    if(err) throw err;
+
+    users = db.collection('users');
+//    collection.insert({a:2}, function(err, docs) {
+
+//      collection.count(function(err, count) {
+//        console.log(format("count = %s", count));
+//      });
+
+      // Locate all the entries using find
+//      collection.find({name:'harm'}).toArray(function(err, results) {
+//        console.dir(results);
+        // Let's close the db
+//        db.close();
+//      });
+//    });
+  });
